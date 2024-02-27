e9fb6967b68201af64f5087c84035a598752494e
shivasingh0 <shiva2003rajawat@gmail.com>
2024-02-28 00:03:13 +0530

    user data stored in database successdully

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { MyContext } from "../../context/data/MyContext";
 import { toast } from "react-toastify";
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebase';
+import { auth, fireDB } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection } from "firebase/firestore";
 
 function Signup() {
   const [name, setName] = useState("");
@@ -13,15 +14,33 @@ function Signup() {
   const context = useContext(MyContext);
   const { loading, setLoading } = context;
 
-  const signup = () => {
+  const signup = async () => {
+    setLoading(true)
     if (name === "" || email === "" || password === "") {
         toast.error("All fields required")
     }
 
     try {
-        const users = createUserWithEmailAndPassword(auth, email, password)
-        console.log(users);
+        const users = await createUserWithEmailAndPassword(auth, email, password)
+        
+        // extract details from users
+        const user = {
+          name : name,
+          uid : users.user.uid,
+          email : users.user.email,
+          time : Timestamp.now()
+        }
+
+        // store details to the collection in dataBase
+        const useRef = collection(fireDB, 'users')
+        await addDoc(useRef, user)
+        toast.success("Signup Successfull!")
+        setName("")
+        setEmail("")
+        setPassword("")
+        setLoading(false)
     } catch (error) {
+       setLoading(false)
         console.log(error);
     }
 
@@ -29,6 +48,7 @@ function Signup() {
 
   return (
     <div className=" flex justify-center items-center h-screen">
+    {loading && <Loader/>}
       <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
         <div className="">
           <h1 className="text-center text-white text-xl mb-4 font-bold">
